6c747347240e1872fb518366a588ade93d8a312e
Rano <khasanova.rano@gmail.com>
2020-06-14 20:16:04 +0600

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        speed: 1200,
+        fade: true,
+        slideToShow: 1,
+        dots: true,
+        arrows: false
+    });
+});
