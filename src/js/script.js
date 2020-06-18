f1cb0426866bda4bcd35e2c686a6d56a6388ee86
dannyblck <Amirkhanov.Official@gmail.com>
2020-06-18 18:37:04 +0600

    Ready

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.carousel').slick({
-        speed: 1200,
-        fade: true,
-        slideToShow: 1,
-        dots: true,
-        arrows: false
-    });
-});
